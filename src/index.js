5e5ad02908bbf1f3d606d36b413f8c9cfbe94c90
Igor Siamykin <sparkisisss@gmail.com>
2023-10-27 23:38:54 +0300

    27-10-23

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import './index.html';
 import './index.scss';
 import "@babel/polyfill";
-import { num, sum } from './modules/calc';
+import './modules/menuBurger'
 
 
-console.log(num(2, 4));
-console.log(sum(2, 4));
+
