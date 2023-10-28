b7610c74d9a7b6b89ae76d3bc6e3d638e4f55c31
Igor Siamykin <sparkisisss@gmail.com>
2023-10-28 20:06:24 +0300

    header complit

--- src/modules/menuBurger.js
+++ src/modules/menuBurger.js
@@ -1,2 +1,12 @@
 "use strict"
 
+const menuBurger = document.querySelector('.burger__point'),
+      menu = document.querySelector('.point'),
+      anyScroll = document.querySelector('body');
+
+menuBurger.addEventListener('click', () => {
+    menuBurger.classList.toggle('active');
+    menu.classList.toggle('active');
+    anyScroll.classList.toggle('lock');
+    console.log('click');
+});
