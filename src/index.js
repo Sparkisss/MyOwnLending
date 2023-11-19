821e7ba9c50c834ba6ba1df06c70fe06d73df6a9
Igor Siamykin <sparkisisss@gmail.com>
2023-11-19 19:45:53 +0300

    lending

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import './modules/showeInfo';
 import './modules/constructor';
 import './modules/get';
 import './modules/weatherAPI';
+import './modules/perspective3d';
+import './modules/rain';
 
 
 
